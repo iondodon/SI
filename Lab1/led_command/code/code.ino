59793ef0d4c3eee0896d12199c441cbdb30e584b
Ion Dodon <iondodon2@gmail.com>
2020-02-08 09:55:26 -0800

    led command

---
+++ Lab1/led_command/code/code.ino
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#define LED_PIN 12
+
+String command;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  command = Serial.readString();
+
+  if(command.length()) {
+    Serial.println(command);
+    if(command == "led on") {
+      digitalWrite(LED_PIN, HIGH);
+      Serial.println("LED is on");
+    } else 
+    if(command == "led off") {
+      digitalWrite(LED_PIN, LOW);
+      Serial.println("LED is off");
+    } 
+  }
+}
