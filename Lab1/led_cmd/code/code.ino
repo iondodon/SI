b772ca4ac66d5eb47826c41e2e9ec5f55c3df325
Ion Dodon <iondodon2@gmail.com>
2020-02-13 15:09:25 +0200

    improvement

--- Lab1/led_cmd/code/code.ino
+++ Lab1/led_cmd/code/code.ino
@@ -1,55 +1,18 @@
 #include <stdio.h>
+#include <Arduino.h>
 #include "mystdio.h"
 
 #define LED_PIN 12
-static FILE *f;
 
-static int putChar(char c, FILE *stream)
-{
-    Serial.write(c) ;
-    return 0 ;
-}
-
-static char getChar(FILE *stream)
-{
-    char c;
-    while(Serial.available()) {  
-       c = Serial.read();
-       printf("%c", c);  
-    }
-    return c;
-}
-
-String readString() {
-  char c;
-  String str;
-  do {
-    scanf("%c", &c);
-    if(c != 0 && c != '\r'){
-      str.concat(c);
-    }
-  }while(c!= '\r');
-
-  return str;
-}
-
-void writeString(String str) {
-  for(int i = 0; i < str.length(); i++) {
-    printf("%c", str[i]);
-  }
-}
+Mystdio io;
+char c;
 
 void setup() {
-  Serial.begin(9600) ;
-  
-  f = fdevopen(putChar, getChar);
-  stdout = f;
-  stdin = f;
+  Serial.begin(9600);
 
-  printf("3\r");
+  printf("1\r");
 }
 
 void loop() {
-  String str = readString();
-  writeString(str);
+  
 }
--- Lab1/led_cmd/code/mystdio.h
+++ Lab1/led_cmd/code/mystdio.h
@@ -1,4 +1,15 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+static FILE *f;
+
+class Mystdio {
+  public:
+    Mystdio();
+    String readStr();
+    void writeStr(String str);
+    static int putChar(char c, FILE *stream);
+    static char getChar(FILE *stream);
+};
+
 #endif
