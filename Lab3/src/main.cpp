fc34bd5149b6f8ebeb7d2994c12f68937f300c5a
Ion Dodon <iondodon2@gmail.com>
2020-04-08 19:20:54 +0200

    added mystdio to lab 3

--- Lab3/src/main.cpp
+++ Lab3/src/main.cpp
@@ -2,19 +2,22 @@
 #include "L298N.h"
 #include "LAMP.h"
 #include "LiquidCrystal.h"
+#include "mystdio.h"
 
 L298N motor(A1, 6, 5);
 LAMP lamp(7);
 LiquidCrystal lcd(10);
+Mystdio mystdio;
 
 
 void setup() {
-  Serial.begin(9600);
+  mystdio.open(StreamIO::SERIALIO);
 
   motor.setSpeed(200);
   motor.backward();
 
   lcd.begin(16, 2);
+  printf("Started");
   lcd.print("Started");
 }
 
