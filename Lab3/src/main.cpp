fafbc58e1c8df0dc5489e50f44f8e3faf073eeae
Ion Dodon <iondodon2@gmail.com>
2020-04-08 19:02:22 +0200

    lcd added to lab3

--- Lab3/src/main.cpp
+++ Lab3/src/main.cpp
@@ -1,13 +1,21 @@
 #include <Arduino.h>
 #include "L298N.h"
 #include "LAMP.h"
+#include "LiquidCrystal.h"
 
-L298N motor(A1, 11, 10);
-LAMP lamp;
+L298N motor(A1, 6, 5);
+LAMP lamp(7);
+LiquidCrystal lcd(10);
 
 
 void setup() {
   Serial.begin(9600);
+
+  motor.setSpeed(200);
+  motor.backward();
+
+  lcd.begin(16, 2);
+  lcd.print("Started");
 }
 
 
