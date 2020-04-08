61361a003f43e53520e9ed9cb25bc37d9dfd1ef3
Ion Dodon <iondodon2@gmail.com>
2020-04-08 18:05:30 +0200

    some work on lab3

--- Lab3/src/main.cpp
+++ Lab3/src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
+#include "L298N.h"
+#include "LAMP.h"
+
+L298N motor(A1, 11, 10);
+LAMP lamp;
+
 
 void setup() {
-  
+  Serial.begin(9600);
 }
 
-void loop() {
 
+void loop() {
+  lamp.setOn();
+  delay(3000);
+  lamp.setOff();
+  delay(3000);
 }
